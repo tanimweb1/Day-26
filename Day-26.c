396fedc8d3b68240a0fab0e0b79eac8cded2292b
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-01-13 00:51:58 +0600

    Update Day-26.c

--- Day-26.c
+++ Day-26.c
@@ -41,9 +41,40 @@ printf("Total: R$ %.2f\n", pay);
 }
 
 
+
+
+
+
+
+/ bee 1040
+
+    float n1,n2,n3,n4,avg;
+scanf("%f%f%f%f",&n1,&n2,&n3,&n4);
+
+avg = (n1*2+n2*3+n3*4+n4*1)/10;
+printf("Media: %.1f\n",avg);
+
+if(avg>=7.0){
+    printf("Aluno aprovado.\n");
+}
+
+if(avg<5){
+    printf("Aluno reprovado.\n");
+}
+
+if(avg>=5.0 && avg<7){
+    printf("Aluno em exame.\n");
+
+
+
+
+
+ 
+
     return 0;
 
 }
+
 
 
 
